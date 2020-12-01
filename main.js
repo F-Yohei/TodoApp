6db0b0472774083ddbc6a0e1b3a3ff0f0cf706ad
Yohei Fukudome <pt83s0502@gmail.com>
2020-12-01 18:01:34 +0900

    削除機能の追加

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ addTaskButton.addEventListener('click', (e) => {
         todos.push(todoInput);
         textBox.focus();
     }
+    console.log(todos);
     addTask();
 });
 
@@ -40,7 +41,7 @@ const addTask = () => {
         comment.innerHTML = todo.task;
 
         stateTd.appendChild(createStateButton());
-        deleteTd.appendChild(createDeleteButton());
+        deleteTd.appendChild(createDeleteButton(id));
     });
 };
 
@@ -52,8 +53,15 @@ const createStateButton = () => {
 };
 
 //ToDoを削除するボタンを作る為の関数
-const createDeleteButton = () => {
+const createDeleteButton = (id) => {
     const deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = '削除';
+    deleteBtn.addEventListener('click', () => {
+        if (id > -1) {
+            todos.splice(id, 1);
+        }
+        addTask();
+        console.log(todos);
+    });
     return deleteBtn;
 };
